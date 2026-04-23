e2f26d0d69a155e7bb3af275d788eb10fa45a57f
Lucas Fresnedas <92407228+LucasFresnedas@users.noreply.github.com>
2026-04-23 11:21:52 -0300

    Implementação do Swagger /3
    Adição do swagger na camada de segurança.
    
    Update

--- src/main/java/com/example/avaliadorfilmes/config/security/SecurityConfig.java
+++ src/main/java/com/example/avaliadorfilmes/config/security/SecurityConfig.java
@@ -32,7 +32,8 @@ public class SecurityConfig {
                         .requestMatchers("/auth/**",
                                 "/usuarios/registrar",
                                 "/swagger-ui/**",
-                                "/swagger-ui.html").permitAll()
+                                "/swagger-ui.html",
+                                "/v3/api-docs/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
